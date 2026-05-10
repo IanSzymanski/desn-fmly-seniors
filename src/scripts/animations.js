17a3896737c228206740685088e306beea92cce7
Ian W. Szymanski <iszymanski@ewu.edu>
2026-05-10 22:08:43 +0000

    gsap pages

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -6,3 +6,9 @@ gsap.from(".card", {
     stagger: 0.15 
 }
 );
+gsap.from(".page-image, .page-name, .favorite-emoji, .page-links, .page-bio, .b2h", { 
+    opacity: 0, 
+    duration: 0.4,
+    stagger: 0.15 
+}
+);
